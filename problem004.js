8c38acd143a21adb926a0298b1a87f3b210c404c
Miłosz Jakubaszek <milosz.jakubaszek@privhost.pl>
2021-10-24 17:06:22 +0200

    Added solution to 'problem004' exercise.

---
+++ problem004.js
@@ -0,0 +1,10 @@
+const getTotalNumberOfNewYaersEveSundays = function (a, b) {
+    let year = a;
+    let counter = 0;
+    for (year; year<= b; year++) {
+        let date = new Date(year, 0, 1);
+        if (date.getDay() === 0)
+                counter++;
+    }
+    return counter
+}
