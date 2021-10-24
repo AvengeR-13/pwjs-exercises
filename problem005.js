88f162bfe9cdac96662d9d4c403fdf50fc773939
Miłosz Jakubaszek <milosz.jakubaszek@privhost.pl>
2021-10-24 17:43:54 +0200

    Added solution for 'problem005' exercise.

---
+++ problem005.js
@@ -0,0 +1,9 @@
+let strangeSum = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return null
+    } else if (a === b) {
+        return  3 * (a + b);
+    } else {
+        return (a + b)
+    }
+}
